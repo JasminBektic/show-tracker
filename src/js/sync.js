07dbab58abce8eec7ec30c6cd8b663cbd3786303
JasminBektic <jaskio89@gmail.com>
2019-01-22 23:40:01 +0100

    sync implemented

--- src/js/sync.js
+++ src/js/sync.js
@@ -13,37 +13,39 @@ console.log((Date.now() - storage.synced) / 1000);
         // Sync every 2 hours or trigger on specific datetime
         // Clone current storage shows then activate sync where limit will be 40req/sec
 
-        if (storage.sync_ids.length > 0) {
-           
-        }
-
         // if ((storage.synced - Date.now()) / 1000/60/60 > 2) {  // sync every 2 hours
-        if ((Date.now() - storage.synced) / 1000 > 5) {  // sync every 2 hours
-            // let storage = await Storage.get();
-
+        if ((Date.now() - storage.synced) / 1000 > 10) {  // sync every 2 hours
+            // Prepare sync ids
             if (storage.sync_ids.length == 0) {
-                await Storage.setKey(SHOWS)
-                             .destroyMultiple();
-
                 storage.shows.forEach((show) => {
                     storage.sync_ids.push({
                         id: show.id,
                         imdb_id: show.imdb_id
                     });
                 });
+                storage.shows = [];
+            }
 
-                let temp_sync = storage.sync_ids.slice();
+            Storage.setKey(SHOWS);
 
-                temp_sync.forEach(async (sw) => {
-                    let show = await Api.getById(sw.id);
-                    show.imdb_id = sw.imdb_id
-                    await Storage.insert(show);
-                    storage.sync_ids.shift();
-                });
-
+            // Get new show data
+            // TODO: limit requests to 40
+            let temp_sync = storage.sync_ids.slice();
+            for (let sw of temp_sync) {
+                let show = await Api.getById(sw.id);
+                show.imdb_id = sw.imdb_id;
+                storage.shows.push(Storage.prepareShowStructure(show));
+                storage.sync_ids.shift();
+            }
+            
+            // Sync all or partial data, depend on request limit
+            if (storage.sync_ids.length == 0) {
                 storage.synced = Date.now();
-
                 await Storage.sync(storage);
+            } else {
+                for (let show of storage.shows) {
+                    await Storage.insert(show);
+                }
             }
         }
     }
